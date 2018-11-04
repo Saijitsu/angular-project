1ef8b0e0de67053b7432f90554f630460063baae
Nicolas Turlet <35563943+Saijitsu@users.noreply.github.com>
2018-11-04 12:55:25 +0100

    update/ etape 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular-project';
+    isAuth = false;
+    
+    appareilOne = 'Machine à laver';
+    appareilTwo = 'Frigo';
+    appareilThree = 'Ordinateur';
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  onAllumer() {
+    console.log('On allume tout !');
+}
 }
