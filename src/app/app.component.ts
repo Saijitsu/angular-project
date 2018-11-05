32b7975d7a97f21abbce11a9085d6301449af727
Nicolas Turlet <35563943+Saijitsu@users.noreply.github.com>
2018-11-05 16:39:00 +0100

    Update test/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,22 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-    isAuth = false;
-    
-    appareilOne = 'Machine à laver';
-    appareilTwo = 'Frigo';
-    appareilThree = 'Ordinateur';
+  isAuth = false;
+
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
 
   constructor() {
     setTimeout(
